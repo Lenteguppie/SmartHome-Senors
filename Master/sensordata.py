dbc2fdd1639a48996967cdb9af07317d70503889
Lenteguppie <saschavis@gmail.com>
2020-04-24 16:21:52 +0200

    Parse data that is coming from thingspeak
    Now able to parse data that is received from thingspeak.

--- Master/sensordata.py
+++ Master/sensordata.py
@@ -2,40 +2,69 @@
 from thingspeak import *
 import json
 
-# class Packet(object):
-#     def __init__(self, payload):
-#         packet_data = JSONParser(payload)
-#         self.name = packet_data.name
-#         self.ID = packet_data.id
-#         self.Value = packet_data.value
+import urllib3
+import time
+
+
+READ_API_KEY='YOUR READ API KEY'
+CHANNEL_ID= 'YOUR CHANNEL ID'
+
+class Packet(object):
+    def __init__(self, payload):
+        print (payload)
+        self.created_at = payload["created_at"]
+        self.entry_id = payload["entry_id"]
+        self.name = payload["field1"]
+        self.id = payload["field2"]
+        self.value = payload["field3"]
+    
+    def print(self):
+        print("Packet --> \"Name: {0}, ID: {1}, Value: {2}\"".format(self.name, self.id, self.value))
 
 class Room(object):
     def __init__(self, room_data):
         self.name = room_data['name']
         self.LDR = room_data['ldr_id']
-        self.DHT = room_data['dht_id']
         self.darkIntensityValue = room_data['darkIntensityValue']
         self.lightIntensityValue = room_data['ligtIntensityValue']
     
     def print(self):
-        print("name: {0}, LDR_ID: {1}, DHT_ID: {2}, Dark: {3}, Light: {4}".format(self.name, self.LDR, self.DHT, self.darkIntensityValue, self.lightIntensityValue))
-
-
+        print("Room --> \"name: {0}, LDR_ID: {1}, Dark: {2}, Light: {3}\"".format(self.name, self.LDR, self.darkIntensityValue, self.lightIntensityValue))
+    
 def get_room_data():
     with open('room.json') as json_file:
         room_data = json.load(json_file)
         print('Name: ' + room_data['name'])
         print('LDR_ID: ' + room_data['ldr_id'])
-        print('DHT_ID: ' + room_data['dht_id'])
         print('darkIntensityValue: ' + str(room_data['darkIntensityValue']))
         print('ligtIntensityValue: ' + str(room_data['ligtIntensityValue']))
         print('')
         return room_data
 
+def get_last_packet():
+
+    http = urllib3.PoolManager()
+    url = "http://api.thingspeak.com/channels/{}/feeds/last.json?api_key={}".format(CHANNEL_ID,READ_API_KEY)
+    r = http.request('GET', url)
+    data = None
+    data_len = 0
+
+    data = json.loads(r.data.decode('utf-8'))
+    data_len = len(data)
+
+    return Packet(data)
 
 slaapkamer = Room(get_room_data())
 slaapkamer.print()
 
+while True:
+    new_packet = get_last_packet()
+    print(new_packet.id)
+    if (new_packet != None ):
+        if (new_packet.id == slaapkamer.LDR):
+            print("slaapkamer LDR :D")
+
+    time.sleep(5)
 
 
 '''
